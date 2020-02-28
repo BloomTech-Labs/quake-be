0b7bc42e63e9a9929c83dfc0cda1d0645fa78c02
Ben Q <Quackenbush117@gmail.com>
2020-02-27 21:05:25 -0700

    server test complete

--- index.js
+++ index.js
@@ -3,28 +3,10 @@ const server = require('./api/server.js')
 
 const PORT = process.env.PORT || 3000;
 
-// server.get('/', (req, res) => {
-//     res.send(`
-//     <div>
-//         <h1>Welcome to Quake Live</h1>
-      
-//     </div>
-//     `)
-// })
-
 server.get("/", (req, res) => {
   res.status(200).json({ api: "up" });
 });
 
-
-// -- final middleware --
-// server.use(function (req, res) {
-//     res.status(404).send(`
-//     <div>
-//         <h1>that page doesnt exist..</h1>
-//     </div>
-//     `);
-// })
 
 server.listen(PORT, () => {
     console.log(`********//   API OPEN ON PORT ${PORT}  //********`)
