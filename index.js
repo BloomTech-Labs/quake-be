50d198b64e74239b34898a8c0fb31d39105a2590
thaveesit douangaphaivong <d15.landon@gmail.com>
2020-03-10 20:32:42 -0500

    auth-model added knex fix login reg tests

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ const server = require('./api/server.js')
 
 const PORT = process.env.PORT || 3000;
 
+server.get('/', (req, res) => {
+    res.send(`
+    <div>
+        <h1>Welcome to Quake Live!</h1>
+      
+    </div>
+    `)
+})
+
+// -- final middleware --
+server.use(function (req, res) {
+    res.status(404).send(`
+    <div>
+        <h1>that page doesnt exist..</h1>
+    </div>
+    `);
+})
+
 
 server.listen(PORT, () => {
     console.log(`********//   API OPEN ON PORT ${PORT}  //********`)
