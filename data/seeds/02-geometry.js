1a8652325a3b6469be13f00493349e1949fa299b
Jeff Breig <jjbreig@gmail.com>
2020-07-07 22:48:28 -0400

    bug fix in seed file

--- data/seeds/02-geometry.js
+++ data/seeds/02-geometry.js
@@ -2,20 +2,20 @@ exports.seed = function(knex) {
     return knex('geometry').insert([
         {
             "type":"Point", 
-            "coordinates": [
+            "coordinates": `[
                 -116.8003333,
                 33.4768333,
                 6.84
-            ],
+            ]`,
             "usgs_id": "ci39492872"
         },
         {
             "type":"Point", 
-            "coordinates": [
+            "coordinates": `[
                 -116.8003333,
                 33.4768333,
                 6.84
-            ],
+            ]`,
             "usgs_id": "ci39492872"
         }
     ]);
