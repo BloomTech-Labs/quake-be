c1afb577fbbeaee7c467879dac8b72ca1da50f0d
Jeff Breig <jjbreig@gmail.com>
2020-07-07 23:30:05 -0400

    combined geometry and activity tables with join

--- activity/activity-model.js
+++ activity/activity-model.js
@@ -1,8 +1,10 @@
 const db = require('../data/db-config');
 
 function findActivity() {
-    return db('activity');
-}
+    return db('activity')
+        .join("geometry", "geometry.usgs_id", "activity.usgs_id")
+        .select("*")
+};
 
 module.exports = {
     findActivity,
