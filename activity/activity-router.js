f9e183ca031e01d6348e03853ed75a62957ea78b
PeteDram <me@petejames.com>
2020-07-18 12:04:48 -0700

    created new endpoints /alltime-biggest and /first-load and adjusted cron job schedule

--- activity/activity-router.js
+++ activity/activity-router.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const axios = require('axios');
-
 const Activity = require('./activity-model.js');
 const cron = require('node-cron');
 
 
-
+// '/api/activity'
 const router = express.Router();
 
-// '/api/activity'
 
-router.get("/", async (req, res) => {
+// '/api/activity/first-load
+router.get("/first-load", async (req, res) => {
   let features = await Activity.findActivity()
     try {
      let newQuakes =  features.map( async (feature) => {
@@ -38,8 +37,8 @@ router.get("/", async (req, res) => {
   }
 });
 
-//first load cron job
-cron.schedule('*/10 * * * * *', () => {
+//first-load cron job
+cron.schedule('0 */5 * * * *', () => { //runs every 5 minutes
     axios.get('https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&limit=30')
     .then(async response=>{
       const countEx = await Activity.countRecords('activity')
@@ -47,9 +46,9 @@ cron.schedule('*/10 * * * * *', () => {
       const countRes = response.data.features.length;
 
       if (countRes == countExisting) {
-        console.log('same number of records in response as in db') //therefore do nothing
+        console.log('first-load: same number of records in response as in db, nothing to change in DB') //therefore do nothing
       } else {
-        console.log('different number of records in response as in db')
+        console.log('first-load: different number of records in response as in db, writing new DB')
         //wipe existing table
         const num2 = await Activity.delAllRecords('geometry')
         console.log('geometry', num2)
@@ -57,7 +56,6 @@ cron.schedule('*/10 * * * * *', () => {
         console.log('activity', num1)
 
         //add new response to table
-
         let newFeatures=response.data.features.map(feature=>{ 
           feature.properties.usgs_id=feature.id;
           feature.geometry.usgs_id=feature.id
@@ -72,8 +70,37 @@ cron.schedule('*/10 * * * * *', () => {
     });
 });
 
-//all time biggest
-cron.schedule('*/15 * * * * *', () => {
+// '/api/activity/alltime-biggest
+router.get("/alltime-biggest", async (req, res) => {
+  let features = await Activity.findActivity()
+    try {
+     let newQuakes =  features.map( async (feature) => {
+        // --- For Each Feature (Earthquake)
+        let featureComplete = {...feature};
+        // Finding and assigning new geometry to each earthquake.
+        let geo = await Activity.findGeometry(feature.usgs_id)
+        //Assigning feature complete the new quake properties... 
+        featureComplete = { 
+          ...feature, 
+          geometry: geo[0]        
+        };
+        // We need this line to parse the coordinates back into an array.
+        featureComplete.geometry.coordinates = JSON.parse(geo[0].coordinates);
+        return featureComplete
+      });
+    //Resolving promises and returning data.
+    let quakeData = await Promise.allSettled(newQuakes)
+    res.json({quakeData});
+  } 
+  
+  catch (error) {
+    console.log(error.message)
+    res.status(500).json({ message: "Failed to get quakes" });
+  }
+});
+
+//all time biggest cron job
+cron.schedule('0 0 0 * * *', () => { //runs everyday at midnight server time.
   axios.get('https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&limit=30')
   .then(async response=>{
     const countEx = await Activity.countRecords('all_time')
@@ -81,9 +108,9 @@ cron.schedule('*/15 * * * * *', () => {
     const countRes = response.data.features.length;
 
     if (countRes == countExisting) {
-      console.log('same number of records in response as in db') //therefore do nothing
+      console.log('alltime-biggest: same number of records in response as in db, nothing to change in DB') //therefore do nothing
     } else {
-      console.log('different number of records in response as in db')
+      console.log('alltime-biggest: different number of records in response as in db, writing new DB') //therefore write new db
       //wipe existing table
       const num2 = await Activity.delAllRecords('geometry_all_time')
       console.log('geometry', num2)
