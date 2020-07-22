eba5c2ec39a40c3903040bf53477f01d6141a664
PeteDram <me@petejames.com>
2020-07-21 18:21:07 -0700

    wip tsunami

--- data/migrations/20200623190235_create-tables.js
+++ data/migrations/20200623190235_create-tables.js
@@ -79,6 +79,45 @@ exports.up = function(knex, Promise) {
             .notNullable().unique()
             .references('usgs_id').inTable('all_time')
     })
+    .createTable("tsunami", activity=> {
+        activity.increments();
+        activity.string("usgs_id").unique();
+        activity.decimal("mag");
+        activity.string("place");
+        activity.bigInteger("time");
+        activity.bigInteger("updated");
+        activity.integer("tz");
+        activity.string("url");
+        activity.string("detail");
+        activity.integer("felt");
+        activity.decimal("cdi");
+        activity.decimal("mmi");
+        activity.string("alert");
+        activity.string("status");
+        activity.integer("tsunami");
+        activity.integer("sig");
+        activity.string("net");
+        activity.string("code");
+        activity.string("ids");
+        activity.string("sources");
+        activity.string("types");
+        activity.integer("nst");
+        activity.decimal("dmin");
+        activity.decimal("rms");
+        activity.decimal("gap");
+        activity.string("magType");
+        activity.string("type");
+        activity.string("title");
+    })
+
+    .createTable("geometry_tsunami", geometry=>{
+        geometry.increments();
+        geometry.string("type");
+        geometry.string("coordinates");
+        geometry.string("usgs_id")
+            .notNullable().unique()
+            .references('usgs_id').inTable('activity')
+    })
 };
 
 exports.down = function(knex) {
