0935c4c24177def7e0b7ebd8548fdc0f043af8d4
PeteDram <me@petejames.com>
2020-07-21 18:47:32 -0700

    added endpoint and cron job for nukes

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const helmet = require('helmet');
 const server = express();
 
 const ActivityRouter = require('./activity/activity-router')
+const NukesRouter = require('./activity/nukes-router')
+
 
 server.use(helmet());
 server.use(express.json());
 server.use('/api/activity',ActivityRouter);
+server.use('/api/nukes', NukesRouter);
 
 server.get('/', (req, res) => {
     res.status(201).json({ message: 'hello world' });
