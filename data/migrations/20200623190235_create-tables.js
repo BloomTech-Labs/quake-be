c303021fd2dacbf9ce51339f75857848dbed4df4
PeteDram <me@petejames.com>
2020-07-22 18:25:29 -0700

    added tsunami tables to migrations

--- data/migrations/20200623190235_create-tables.js
+++ data/migrations/20200623190235_create-tables.js
@@ -109,7 +109,6 @@ exports.up = function(knex, Promise) {
         activity.string("type");
         activity.string("title");
     })
-
     .createTable("geometry_nukes", geometry=>{
         geometry.increments();
         geometry.string("type");
@@ -118,14 +117,50 @@ exports.up = function(knex, Promise) {
             .notNullable().unique()
             .references('usgs_id').inTable('activity')
     })
-
-    // .createTable("tsunami", activity=> {
-
-    //     .createTable("geometry_tsunami", geometry=>{
+    .createTable("tsunami", activity=> {
+        activity.increments();
+        activity.string("usgs_id").unique();
+        activity.decimal("mag");
+        activity.string("place");
+        activity.bigInteger("time");
+        activity.bigInteger("updated");
+        activity.integer("tz");
+        activity.string("url");
+        activity.string("detail");
+        activity.integer("felt");
+        activity.decimal("cdi");
+        activity.decimal("mmi");
+        activity.string("alert");
+        activity.string("status");
+        activity.integer("tsunami");
+        activity.integer("sig");
+        activity.string("net");
+        activity.string("code");
+        activity.string("ids");
+        activity.string("sources");
+        activity.string("types");
+        activity.integer("nst");
+        activity.decimal("dmin");
+        activity.decimal("rms");
+        activity.decimal("gap");
+        activity.string("magType");
+        activity.string("type");
+        activity.string("title");
+    })
+    .createTable("geometry_tsunami", geometry=>{
+        geometry.increments();
+        geometry.string("type");
+        geometry.string("coordinates");
+        geometry.string("usgs_id")
+            .notNullable().unique()
+            .references('usgs_id').inTable('activity')
+    })
 };
 
 exports.down = function(knex) {
     return knex.schema
+    .dropTableIfExists("geometry_tsunami")
+    .dropTableIfExists("tsunami")
     .dropTableIfExists("geometry_nukes")
     .dropTableIfExists("nukes")
     .dropTableIfExists("geometry_all_time")
