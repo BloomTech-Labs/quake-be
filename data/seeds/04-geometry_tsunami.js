805d70a35e586d27f0de972eb83b1a759aace793
PeteDram <me@petejames.com>
2020-07-22 18:54:02 -0700

    seed added

---
+++ data/seeds/04-geometry_tsunami.js
@@ -0,0 +1,13 @@
+exports.seed = function(knex) {
+    return knex('geometry_tsunami').insert([
+    {
+        "type": "Point",
+        "coordinates": [
+            142.373,
+            38.297,
+            29
+        ],
+        "usgs_id": "official20110311054624120_30"
+        }
+    ]);
+};
