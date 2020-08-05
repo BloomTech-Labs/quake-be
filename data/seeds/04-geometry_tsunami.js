7df9ddf635903cd776aa6a281e97be5f274a54c4
PeteDram <me@petejames.com>
2020-08-04 20:55:10 -0700

    added more tsunamis

--- data/seeds/04-geometry_tsunami.js
+++ data/seeds/04-geometry_tsunami.js
@@ -10,13 +10,31 @@ exports.seed = function(knex) {
         "usgs_id": "official20110311054624120_30"
         },
         {
+        "type": "Point",
+            "coordinates": `[
+                -72.898,
+                -36.122,
+                22.9
+            ]`,
+            "usgs_id": "official20100227063411530_30"
+        },
+        {
             "type": "Point",
             "coordinates": `[
-                142.373,
-                38.297,
-                29
+                95.982,
+                3.295,
+                30
             ]`,
-            "usgs_id": "official20110311054624120_31"
-            },
+            "usgs_id": "official20041226005853450_30"
+        },
+        {
+            "type": "Point",
+            "coordinates": `[
+                -147.339,
+                60.908,
+                25
+            ]`,
+            "usgs_id": "official19640328033616_30"
+        },
     ]);
 };
